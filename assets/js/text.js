4661b1b312637c3a0af0ad54f9c4eaf0fe1e51b9
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-05-17 13:32:32 +0530

    Lower the time duration for a single period
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- assets/js/text.js
+++ assets/js/text.js
@@ -21,7 +21,7 @@ $(function() {
 $(function() {
   $(".typed").typed({
       strings: ['Contributing to Evolution X ', "Computer Science Student ", "@HemantSachdeva", "He Code When He's Bored ", "18 y.o "],
-      typeSpeed: 150,
+      typeSpeed: 50,
       loop: true,
       cursorChar: "|",
   });
