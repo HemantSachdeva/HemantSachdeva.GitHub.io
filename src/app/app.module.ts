345afd917a968f646c1358083e870c6343a65181
Hemant Sachdeva <hemant.evolver@gmail.com>
2026-01-12 00:49:04 +0530

    Remove PWA
    Getting high traffic (from bots I guess).
    
    Partially reverts fd56b3529fbb112457fd93f708d885add440b2e8.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeModule } from './components/home/home.module';
-import { GeneralModule } from './components/general/general.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser';
+import { GeneralModule } from './components/general/general.module';
+import { HomeModule } from './components/home/home.module';
+import { NgModule } from '@angular/core';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { environment } from '../environments/environment';
 
 export function HttpLoaderFactory(http: HttpClient){
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -33,7 +32,6 @@ export function HttpLoaderFactory(http: HttpClient){
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     NgxGoogleAnalyticsModule.forRoot(environment.trackAnalyticID),
     TranslateModule.forRoot({
       loader: {
